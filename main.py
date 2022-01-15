26c9e3a9a634d03d6ed96ac1896d954a1662bff7
Angel Penchev <26301867+angel-penchev@users.noreply.github.com>
2022-01-15 21:43:12 +0200

    [Feat] Continuous running
    Made the application run the check every 15 minutes.
    Added config parameters for if to run the check once and run interval.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 import logging
 import os
+from time import sleep
 
 from dotenv import load_dotenv
 
@@ -21,6 +22,8 @@ if __name__ == "__main__":
         2 = successfully filled and unfilled forms notifications,
         3 = closed, filled and unfilled forms notifications"""
     )
+    parser.add_argument("-o", "--run-once", help="whether to check only once and exit", action="store_true")
+    parser.add_argument("-i", "--interval", type=int, help="interval in minutes between checks")
     args = parser.parse_args()
 
     load_dotenv()
@@ -31,6 +34,8 @@ if __name__ == "__main__":
     COURSE_ID = int(args.course or os.getenv("MCHAF_COURSE_ID"))
     CHOICE_TITLE_REGEX = args.regex or os.getenv("MCHAF_CHOICE_TITLE_REGEX")
     NOTIFICATION_LEVEL = int(args.notification_level or os.getenv("MCHAF_NOTIFICATION_LEVEL") or 2)
+    RUN_ONCE = bool(args.run_once or os.getenv("MCHAF_RUN_ONCE") or False)
+    INTERVAL = int(args.interval or os.getenv("MCHAF_INTERVAL") or 15) * 60
 
     if not USERNAME or not PASSWORD or not MOODLE_DOMAIN or not COURSE_ID or not CHOICE_TITLE_REGEX:
         e = AssertionError('Insufficient initialization parameters supplied.')
@@ -39,3 +44,7 @@ if __name__ == "__main__":
 
     app = MCHAF(USERNAME, PASSWORD, MOODLE_DOMAIN, COURSE_ID, CHOICE_TITLE_REGEX, NOTIFICATION_LEVEL)
     app.run()
+
+    while not RUN_ONCE:
+        sleep(INTERVAL)
+        app.run()
