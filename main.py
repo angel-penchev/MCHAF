0b7c59c4de6d0475faa66efba182ef214cb55306
Angel Penchev <26301867+angel-penchev@users.noreply.github.com>
2022-01-15 05:10:03 +0200

    [Feat] Program arguments
    Added optional program arguments for setting startup parameters.
    This servers more config options than only having enviourment variables.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import logging
 import os
 
@@ -6,14 +7,30 @@ from dotenv import load_dotenv
 from src.MCHAF import MCHAF
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", "--username", type=str, help="moodle authentication username")
+    parser.add_argument("-p", "--password", type=str, help="moodle authentication password")
+    parser.add_argument("-d", "--domain", type=str, help="moodle server domain")
+    parser.add_argument("-c", "--course", type=int, help="moodle course id")
+    parser.add_argument("-r", "--regex", type=str, help="choice form title regex filter")
+    parser.add_argument(
+        "-n", "--notification-level", type=int,
+        help="""notification level.
+        0 = no notifications,
+        1 = unfilled multiple choice forms notification,
+        2 = successfully filled and unfilled forms notifications,
+        3 = closed, filled and unfilled forms notifications"""
+    )
+    args = parser.parse_args()
+
     load_dotenv()
 
-    USERNAME = os.getenv("MCHAF_USERNAME")
-    PASSWORD = os.getenv("MCHAF_PASSWORD")
-    MOODLE_DOMAIN = os.getenv("MCHAF_MOODLE_DOMAIN")
-    COURSE_ID = int(os.getenv("MCHAF_COURSE_ID"))
-    CHOICE_TITLE_REGEX = os.getenv("MCHAF_CHOICE_TITLE_REGEX")
-    NOTIFICATION_LEVEL = int(os.getenv("MCHAF_NOTIFICATION_LEVEL")) or 2
+    USERNAME = args.username or os.getenv("MCHAF_USERNAME")
+    PASSWORD = args.password or os.getenv("MCHAF_PASSWORD")
+    MOODLE_DOMAIN = args.domain or os.getenv("MCHAF_MOODLE_DOMAIN")
+    COURSE_ID = int(args.course or os.getenv("MCHAF_COURSE_ID"))
+    CHOICE_TITLE_REGEX = args.regex or os.getenv("MCHAF_CHOICE_TITLE_REGEX")
+    NOTIFICATION_LEVEL = int(args.notification_level or os.getenv("MCHAF_NOTIFICATION_LEVEL") or 2)
 
     if not USERNAME or not PASSWORD or not MOODLE_DOMAIN or not COURSE_ID or not CHOICE_TITLE_REGEX:
         e = AssertionError('Insufficient initialization parameters supplied.')
